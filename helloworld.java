861d73d0a18bba41badfaac6b03c7243605ad0e8
aruns18 <106881064+aruns18@users.noreply.github.com>
2022-06-08 22:20:21 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-public class helloworld1 {
+public class helloworld {
    public static void main(String[] args) {
     System.out.println("Hello World 8th june - 08:32 AM - Arun Devops - Cron");
     }
