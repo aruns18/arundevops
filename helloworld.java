d98cfd2e1cbada81d243bfed26403a5cfb913767
aruns18 <106881064+aruns18@users.noreply.github.com>
2022-06-09 11:56:10 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 8th june - 08:32 AM - Arun Devops - SCM Poll");
+    System.out.println("Hello World 8th june - 08:32 AM - Arun Devops - SCM Poll working but Webhooks is not working with private IP");
     }
 }
